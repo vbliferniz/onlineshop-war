dc49026b9ba7b2a1a1c5a0f159e32b3c16839a93
bliferniz <vitalij.bliferniz@web.de>
2016-08-28 22:49:39 +0200

    Register User using Cookies implemented, JavaBean "Customer" added

--- src/de/java2enterprise/onlineshop/RegisterServlet.java
+++ src/de/java2enterprise/onlineshop/RegisterServlet.java
@@ -1,10 +1,11 @@
 package de.java2enterprise.onlineshop;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,20 +16,22 @@ public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = -3377146157514902441L;
 	
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/html;charset=UTF-8");
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
 		String email = req.getParameter("email");
 		String password = req.getParameter("password");
+
+		Cookie cookieEmail = new Cookie("email", email);
+		res.addCookie(cookieEmail);
 		
-		PrintWriter writer = resp.getWriter();
-		writer.println("<!DOCTYPE html>");
-		writer.println("<html>");
-		writer.println("<body>");
-		writer.println("<br>Ihre eingegebenen Daten:");
-		writer.println("<br>Email: " + email);
-		writer.println("<br>Password: " + password);
-		writer.println("</body>");
-		writer.println("</html>");
+		Cookie cookiePassword = new Cookie("password", password);
+		res.addCookie(cookiePassword);
+		
+		
+		res.setContentType("text/html;charset=UTF-8");
+
+		RequestDispatcher requestDispatcher = req.getRequestDispatcher("index.html");
+		requestDispatcher.forward(req, res);
 	}
 
 }
