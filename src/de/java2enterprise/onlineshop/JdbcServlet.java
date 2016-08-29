b0d6260ccba5783b694513bdbd93023be7f04135
bliferniz <vitalij.bliferniz@web.de>
2016-08-29 22:43:11 +0200

    jdbc.properties added

--- src/de/java2enterprise/onlineshop/JdbcServlet.java
+++ src/de/java2enterprise/onlineshop/JdbcServlet.java
@@ -1,20 +1,44 @@
 package de.java2enterprise.onlineshop;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Properties;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/JdbcServlet")
 public class JdbcServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String driver = request.getServletContext().getInitParameter("driver");
-		response.getWriter().println(driver);
+		
+		String jdbcProperties 	= getInitParameter("jdbc_properties");
+		
+		ServletContext servletContext = getServletContext();
+		InputStream jdbcPropertiesStream = servletContext.getResourceAsStream(jdbcProperties);
+		
+		
+		Properties properties = new Properties();
+		properties.load(jdbcPropertiesStream);
+		
+		response.setContentType("text/html;charset=UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<br>driver: " + properties.getProperty("driver"));
+		out.println("<br>url: " + properties.getProperty("url"));
+		out.println("<br>username: " + properties.getProperty("username"));
+		out.println("<br>password: " + properties.getProperty("password"));
+		out.println("</body>");
+		out.println("</html>");
+		
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
